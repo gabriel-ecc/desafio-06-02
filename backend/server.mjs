d93b0a075e94e22b09c95ca93d12a65417de6b9d
Andreina Guerra <141190940+Gealuque@users.noreply.github.com>
2025-06-01 20:32:30 -0400

    REQ4__REQ5

--- backend/server.mjs
+++ backend/server.mjs
@@ -38,6 +38,40 @@ app.get("/canciones", (req, res) => {
   res.status(200).json(mostrarCanciones());
 });
 
-app.put("/canciones/:id", (req, res) => {});
+//REQ-04
 
-app.delete("/canciones/:id", (req, res) => {});
+app.put("/canciones/:id", (req, res) => {
+  try {
+        const {id} = req.params
+        const {titulo, artista, tono} = req.body
+
+        if(!titulo || !artista|| !tono){
+          return res.status(400).json({ mensaje: 'Todos los campos son obligatorios.' })
+        }
+
+        const canciones = JSON.parse(fs.readFileSync('./data/repertorio.json', 'utf8'))
+        const index = canciones.findIndex(i => i.id == id)  
+        canciones[index] = {...canciones[index], titulo,artista,tono}
+        
+        fs.writeFileSync('./data/repertorio.json', JSON.stringify(canciones))
+        res.json({ mensaje: 'Canción editada!.' })
+    } catch (error) {
+        res.status(500).json({mensaje:'No se pudo borrar la canción',error:error.message})
+    }
+});
+
+//REQ-05
+app.delete("/canciones/:id", (req, res) => {
+      try {
+        const {id} = req.params
+        const canciones = JSON.parse(fs.readFileSync('./data/repertorio.json', 'utf8'))
+        const NuevaListaCanciones = canciones.filter(e => e.id !== id)
+        if(canciones.length === NuevaListaCanciones.length){
+            return res.status(404).json({mensaje:'No se encontró la canción'})
+        }
+        fs.writeFileSync('./data/repertorio.json', JSON.stringify(NuevaListaCanciones))
+        res.json({ mensaje: 'Canción eliminada!.' })
+    } catch (error) {
+        res.status(500).json({mensaje:'No se pudo borrar la canción',error:error.message})
+    }
+});
